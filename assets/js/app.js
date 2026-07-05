6c8494d193446111596e2a73a4be9a6661207085
BurakOeztuncDev <burakoztunc90@gmail.com>
2026-07-06 01:18:06 +0200

    Komplette Website wurde angepasst und aktualisiert.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,6 @@ import Navbar from "./navbar.js";
 import LoadingScreen from "./loadingScreen.js";
 import Particles from "./particles.js";
 import Animations from "./animations.js";
-import Github from "./github.js";
 import ContactForm from "./contactForm.js";
 
 class App {
@@ -13,7 +12,6 @@ class App {
         this.loader = new LoadingScreen();
         this.particles = new Particles();
         this.animations = new Animations();
-        this.github = new Github();
         this.contactForm = new ContactForm();
 
     }
@@ -24,7 +22,6 @@ class App {
         this.loader.init();
         this.particles.init();
         this.animations.init();
-        this.github.init();
         this.contactForm.init();
 
         console.log("Portfolio gestartet 🚀");
